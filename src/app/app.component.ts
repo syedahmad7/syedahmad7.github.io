d1f25a46542b5934e6b53de16fe3b00bfd36ed50
cael <coregan121@gmail.com>
2021-11-23 22:22:13 -0600

    big moves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,12 @@ export class AppComponent {
   public button(){
     alert("I live under your bed but only when you're not there");
   }
+  public bombSynagogue(){
+    var latDec = (Math.random() * 99999) / 10000;
+    var lat = Math.random() * (90 + 90) - 90 + latDec;
+    var longDec = (Math.random() * 99999) / 10000;
+    var long = (Math.random() * (180 + 180) - 180) + longDec;
+
+    alert("Synagogue bombed at: " + lat + ", " + long);
+  }
 }
