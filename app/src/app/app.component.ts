417d6ea8b9852747d73cc823311f7c32dea70c93
Syed Alhussain <syed.alhussain@inmotioncare.com>
2022-08-01 23:19:42 -0500

    oops

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  currentDate: any;
+  targetDate: any;
+  cDateMillisecs: any;
+  tDateMillisecs: any;
+  difference: any;
+  seconds: any;
+  minutes: any;
+  hours: any;
+  days: any;
+  year: number = 2022;
+  month: number = 7;
+  months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'April',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  day: number = 9;
+
+  ngAfterViewInit() {
+    this.myTimer();
+    setInterval(() => {
+      this.myTimer(); // api call
+   }, 1000);
+    
+  }
+
+  myTimer() {
+    this.currentDate = new Date();
+    this.targetDate = new Date(2022, 7, 9);
+    this.cDateMillisecs = this.currentDate.getTime();
+    this.tDateMillisecs = this.targetDate.getTime();
+    this.difference = this.tDateMillisecs - this.cDateMillisecs;
+    this.seconds = Math.floor(this.difference / 1000);
+    this.minutes = Math.floor(this.seconds / 60);
+    this.hours = Math.floor(this.minutes / 60);
+    this.days = Math.floor(this.hours / 24);
+
+    this.hours %= 24;
+    this.minutes %= 60;
+    this.seconds %= 60;
+    this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
+    this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+    this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+
+    // document.getElementById('days').innerText = this.days;
+    // document.getElementById('hours').innerText = this.hours;
+    // document.getElementById('mins').innerText = this.minutes;
+    // document.getElementById('seconds').innerText = this.seconds;
+
+    setInterval(this.myTimer, 1000)
+}
+}
